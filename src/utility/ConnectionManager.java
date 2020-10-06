977ffdf1ee165d1f8ad8f3f6543eb30a1c366624
Anju <anju02051998@gmail.com>
2020-10-06 17:34:29 +0530

    prograd

--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,40 +1,46 @@
 package utility;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+import java.io.InputStream;
+
 public class ConnectionManager {
 	
-	public static Connection getConnection() throws ClassNotFoundException, SQLException
-	{
-		Properties prop=null;
-		try
-		{
-			prop=loadPropertiesFile();
-		}catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-		String driver=prop.getProperty("driver");
-		String url=prop.getProperty("url");
-		String username=prop.getProperty("username");
-		String pwd=prop.getProperty("password");
-		Class.forName(driver);
-		Connection con=DriverManager.getConnection(url,username,pwd);
-		return con;
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
 		
-   }
-	public static Properties loadPropertiesFile() throws IOException
-	{
-		Properties prop=new Properties();
-		InputStream in=ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		//properties object
+		Properties prop = null;
+		try {
+			prop = loadPropertiesFile();
+		} catch (Exception e1) {
+			
+			e1.printStackTrace();
+		}
+		final String driver = prop.getProperty("driver");
+		final String url = prop.getProperty("url");
+		final String username = prop.getProperty("username");
+		final String password = prop.getProperty("password");
+		
+		//Register the driver class
+		Class.forName(driver);
+		
+		//create the connection object
+		Connection con = null;
+		con =DriverManager.getConnection(url,username,password);
+		return con;
+	}
+	
+	  //properties method
+	  public static Properties loadPropertiesFile() throws Exception 
+	   {
+		Properties prop = new Properties();
+		InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
 		prop.load(in);
 		in.close();
-	    return prop;
-		
-	}
+		return prop; 
+	   } 
+	
 }
